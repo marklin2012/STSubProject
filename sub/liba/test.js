3fdd9fbb77d2b8d98c1c60e8c36cb731eaad2971
林艺 <linyi@ECCMAC-C02QFVD7.local>
2018-09-27 17:24:04 +0800

    fix: 去掉 conflict

--- sub/liba/test.js
+++ sub/liba/test.js
@@ -1,11 +1,7 @@
 const { libTest } = require('./sub/libb/test')
 
 function getContent () {
-<<<<<<< HEAD
-    return 'test liba from sub'
-=======
     return `test liba from self >> ${libTest()}`
->>>>>>> c5e5ac14a72b6182bb51e95c8e93f8c0ef8c9366
 }
 
 module.exports = {
